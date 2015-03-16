a95d31597e1479e9a4243bf92a57cd91dcbe04ab
Sean Herron <sean.herron@gsa.gov>
2015-03-16 13:19:56 -0400

    Remapped folder to be in line with Django best practices

--- init.sh
+++ init.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 echo "------ Create database tables ------"
-python manage.py migrate --noinput
+python abb/manage.py migrate --noinput
  
 echo "------ create default admin user ------"
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', '', 'changeme')" | python manage.py shell
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', '', 'changeme')" | python abb/manage.py shell
 
 echo "------- load data ------"
-python manage.py loaddata hours/fixtures/initial_data.json
+python abb/manage.py loaddata hours/fixtures/initial_data.json
 
 echo "------ starting django &nbsp;------"
-waitress-serve --port=$VCAP_APP_PORT abb.wsgi:application
+cd abb && waitress-serve --port=$VCAP_APP_PORT abb.wsgi:application
